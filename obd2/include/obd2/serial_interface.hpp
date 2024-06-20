53da5dcf0ec6d794c5af332fbaaca98eb32e6afc
Daniel Hafner <daniel.hafner90@gmx.de>
2024-06-20 09:05:43 +0000

    Add serial port files

---
+++ obd2/include/obd2/serial_interface.hpp
@@ -0,0 +1,20 @@
+#ifndef OBD2_SERIAL_INTERFACE_HPP
+#define OBD2_SERIAL_INTERFACE_HPP
+
+#include <libserial/SerialPort.h>
+
+namespace obd2::connection
+{
+
+/// \brief
+class SerialInterface
+{
+
+public:
+    /// \brief
+    SerialInterface();
+};
+
+} // namespace obd2::connection
+
+#endif // OBD2_SERIAL_INTERFACE_HPP
---
+++ obd2/src/obd2/serial_interface.cpp
@@ -0,0 +1,8 @@
+#include "obd2/serial_interface.hpp"
+
+namespace obd2::connection
+{
+
+SerialInterface::SerialInterface() {}
+
+} // namespace obd2::connection
